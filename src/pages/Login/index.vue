36e4614e0f7e2804a767ab1f81a461f0c33729be
scg970511 <1951765501@qq.com>
2020-10-20 14:12:53 +0800

    登入结构完成

---
+++ src/pages/Login/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="login-page">
+    <el-form
+      :model="loginForm"
+      status-icon
+      :rules="rules"
+      ref="loginForm"
+      label-width="100px"
+      class="demo-loginForm"
+    >
+      <h1>9527小课堂</h1>
+      <el-form-item label="用户名" prop="username">
+        <el-input
+          type="text"
+          v-model="loginForm.username"
+          autocomplete="off"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input
+          type="password"
+          v-model="loginForm.password"
+          autocomplete="off"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="success" @click="submitForm('loginForm')"
+          >提交</el-button
+        >
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    //jsDoc
+    /**
+     * @param {Object} rule 就是一个表单验证对象
+     * @param {String} value 输入值
+     * @param {Function} callback 校验通过不传参 不通过传参
+     */
+    var validateUsername = (rule, value, callback) => {
+      if (!value) {
+        callback("4到16位(字母,数字,下划线,符号)");
+      } else {
+        callback();
+      }
+    };
+    var validatePassword = (rule, value, callback) => {
+      if (!value) {
+        callback("请输入密码");
+      } else {
+        callback();
+      }
+    };
+    return {
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [{ validator: validateUsername, trigger: "blur" }],
+        password: [{ validator: validatePassword, trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+<style>
+/* 表单样式 */
+.el-form {
+  width: 400px;
+}
+.el-form {
+  width: 400px;
+  height: 460px;
+  opacity: 0.8;
+  background-color: cyan;
+  margin: 100px 0 0 850px;
+  position: absolute;
+}
+.el-form-item {
+  position: relative;
+  top: 150px;
+}
+.el-button {
+  width: 83%;
+  background: linear-gradient(90deg, #1596fb, #002dff);
+}
+h1 {
+  position: relative;
+  text-align: center;
+  top: 100px;
+}
+</style>
